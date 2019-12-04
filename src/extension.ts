2812f9be477f7173826f46bee8747a4359ce0319
Alex Strive <3083076-alexstrive@users.noreply.gitlab.com>
2019-12-04 20:14:00 +0300

    Some experiments with vscode API

--- src/extension.ts
+++ src/extension.ts
@@ -2,28 +2,13 @@ import fs = require('fs');
 import path = require('path');
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "helloworld-sample" is now active!'
-  );
+export const activate = (context: vscode.ExtensionContext) => {
+  const bookmarks = [];
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    'extension.helloWorld',
-    () => {
-      // The code you place here will be executed every time your command is executed
-
-      // Display a message box to the user
-      vscode.window.showInformationMessage('Hello World!');
-    }
-  );
-
-  context.subscriptions.push(disposable);
-}
+  vscode.commands.registerCommand('xbookmarks.helloWorld', () => {
+    vscode.window.showWarningMessage('shit');
+  });
+};
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
